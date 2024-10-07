677849853a556c5494fe909a50c6dc5baf9f707a
Jonny Kalambay <jonnyk20@gmail.com>
2024-10-06 18:37:34 -0700

    Upgrading Node

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const {readFileSync} = require('fs');
-const handlebars = require('handlebars');
 
 const app = express();
 // Serve the files in /assets at the URI /assets.
 app.use('/assets', express.static('assets'));
+
+const APP_VERSION = process.env.VERSION || '0.0.1';
+const NODE_VERSION = process.version;
+
 
 // The HTML content is produced by rendering a handlebars template.
 // The template values are stored in global state for reuse.
@@ -15,28 +18,13 @@ const data = {
 let template;
 
 app.get('/', async (req, res) => {
-  // The handlebars template is stored in global state so this will only once.
-  if (!template) {
-    // Load Handlebars template from filesystem and compile for use.
-    try {
-      template = handlebars.compile(readFileSync('index.html.hbs', 'utf8'));
-    } catch (e) {
-      console.error(e);
-      res.status(500).send('Internal Server Error');
-    }
-  }
-
-  // Apply the template to the parameters to generate an HTML string.
-  try {
-    const output = template(data);
-    res.status(200).send(output);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send('Internal Server Error');
-  }
+  return res.json({
+    appVersion: APP_VERSION,
+    nodeVersion: NODE_VERSION,
+  });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(
     `Hello from Cloud Run! The container started successfully and is listening for HTTP requests on ${PORT}`
